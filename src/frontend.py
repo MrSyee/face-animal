ee840147cbc756829d383b4d1b63fbf3a70dbfbb
Kyunghwan kim <khsyee@gmail.com>
2022-03-15 19:14:28 +0900

    Add frontend to upload image file

---
+++ src/frontend.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from PIL import Image
+
+
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+
+def main():
+    st.title("Upload your Image!")
+
+    image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+    if image_file is not None:
+        # To See details
+        file_details = {"filename":image_file.name, "filetype":image_file.type,
+                        "filesize":image_file.size}
+        st.write(file_details)
+
+        # To View Uploaded Image
+        st.image(load_image(image_file),width=250)
+
+
+if __name__ == "__main__":
+    main()
